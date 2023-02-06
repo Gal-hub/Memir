ce3cd431b0b18e052ff9ee9c27c604ff6d7675bc
Gal-hub <53276335+Gal-hub@users.noreply.github.com>
2023-02-06 19:09:16 +0200

    Add project files.

---
+++ Memirv2/mainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memirv2
+{
+    public class mainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string ip;
+        public string Ip //The username propery for the binding
+        {
+            get { return ip; }
+            set
+            {
+                ip = value;
+                OnPropertyChanged(nameof(Ip));
+            }
+        }
+
+        private string port;
+        public string Port //The username propery for the binding
+        {
+            get { return port; }
+            set
+            {
+                port = value;
+                OnPropertyChanged(nameof(Port));
+            }
+        }
+    }
+}
